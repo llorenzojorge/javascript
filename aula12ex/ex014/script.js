1058866a92cad5e96516404e66517c2972fa18a1
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-09 00:24:03 -0300

    parte 2 do exerc

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = '#19100B'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'tarde.png'
+        document.body.style.background = '#B9846F'
+    } else {
+        // BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = 'blue'
+    }
+}
