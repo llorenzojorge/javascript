6792a694785c3f3950d9ffc25e9a78867a21f41f
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-09 17:21:35 -0300

    resolução do ex14

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,10 +1,9 @@
 function carregar() {
     var msg = document.getElementById('msg')
-    var img = document.getElementById('foto')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
         img.src = 'manha.png'
