f510d5d7c035894d5328042a751882df17529a72
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-15 00:33:50 -0300

    iniciando o ex sozin

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,9 @@
+function contar(){
+    var inicio = document.getElementById('idinicio').Number
+    var fim = document.getElementById('idfim').Number
+    var passo = document.getElementById('idpasso').Number
+    var resultado = document.getElementById('res')
+    var soma = (`idinicio + id / idpasso`)
+    resultado.innerHTML = soma
+    
+} 
