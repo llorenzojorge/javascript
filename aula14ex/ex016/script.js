a78561fb9c5602b9f2e3058041b6b60130a74a78
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-15 13:08:40 -0300

    ajustes

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,9 +1,19 @@
 function contar(){
-    var inicio = document.getElementById('idinicio').Number
-    var fim = document.getElementById('idfim').Number
-    var passo = document.getElementById('idpasso').Number
-    var resultado = document.getElementById('res')
-    var soma = (`idinicio + id / idpasso`)
-    resultado.innerHTML = soma
-    
+    let inicio = document.getElementById('idinicio')
+    let fim = document.getElementById('idfim')
+    let passo = document.getElementById('idpasso')
+    let resultado = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Está faltando Dados')
+    } else {
+        resultado.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f; c += p) {
+            resultado.innerHTML += `${c} `
+        }
+    }
 } 
