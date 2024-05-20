118159120c0d4d015ed7e0725acda204ddae4f6d
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-20 15:40:33 -0300

    ex01 e 02 de logica

---
+++ logica01/script.js
@@ -0,0 +1,9 @@
+        /*E ae Jhonatan, bora dar uma saída hoje?
+        Se eu terminar meu trabalho aqui, eu consigo.*/
+
+        var trabalho_terminado = false
+        if (trabalho_terminado == true) {
+            console.log('Opa, eu vou conseguir ir sim!')
+        } else {
+            console.log('Infelizmente não vai dar...')
+        }
---
+++ logica02/script.js
@@ -0,0 +1,9 @@
+/*Ei, você consegue me ajudar a mover essas caixas lá pra fora hoje a tarde?
+Se eu estiver livre, sim. Mas, se não der pede meu irmaõ para te ajudar*/
+
+let estou_livre = false
+if (estou_livre == false) {
+    console.log('Pede ajuda ao meu irmão, hj eu to off!')
+} else {
+    console.log('Vou ter dar essa moral paizão')
+}
