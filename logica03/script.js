78a5a6683fe1c5f4cdffa5a4ad09d5e72670b085
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-20 16:39:17 -0300

    ex 03 e 04

---
+++ logica03/script.js
@@ -0,0 +1,9 @@
+/*Eu cheguei atrasado na aula, ainda posso entrar?
+Se essa não for sua terceira vez chegando atrasado, então pode sim, caso contrário irá tomar uma suspensão*/
+
+let quantidade_de_atrasos = 2
+if (quantidade_de_atrasos >= 3) {
+    console.log('Pô irmão, tu vai tomar é uma suspensão bobão!')
+} else {
+    console.log('Ta tranquilo pai, pode entrar no bgl')
+}
