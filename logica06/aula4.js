2cfe7cf19552aadc7896400de4e2ff3a5d38a0dc
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-22 20:44:33 -0300

    ex4 resolvido amém

--- logica06/aula4.js
+++ logica06/aula4.js
@@ -1,9 +1,9 @@
-var idades = [18, 19, 20, 21]
+// Dado uma coleção de dados "idades", imprima  na tela a soma desses valores
+
+var idades = [15, 46, 75, 34, 23]
 var total = 0
-var i = 0
 
-while (i < idades.length){
-    total = total + idades[i]
+for (var contador = 0; contador < idades.length; contador++) {
+    total += idades[contador]
+    console.log(total)
 }
-
-console.log(total)
