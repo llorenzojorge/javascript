722c39148654b0dd641cf6f0aad220e616bfdd39
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-05-23 18:02:09 -0300

    teste

---
+++ logica07/ambiente.js
@@ -0,0 +1,8 @@
+/*
+Crie um programa que recebe um número e imprime o seu fatorial.
+Fatorial deve ser positivo e inteiro
+Exemplo de Fatorial de 4 = (4 * 3 * 2 * 1)
+*/
+var n1 = Number(window.prompt('Digite um numero'))
+var n2 = Number(window.prompt('Outro agr'))
+alert(n1 + n2)
