d62a3233858c025db3ca38e96a9c0531f14b6509
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-04 18:32:12 -0300

    5% de erro 1018

---
+++ beecrowd/testes/1018testes.js
@@ -0,0 +1,8 @@
+const nota100 = Math.floor(576 / 100);
+const nota50 = Math.floor((576 % 100) / 50);
+
+const sobras50 = (576 % 100) % 50;
+
+console.log(nota100);
+console.log(nota50);
+console.log(sobras50)
