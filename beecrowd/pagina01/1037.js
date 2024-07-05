36b2b7e02238e8e0cbf81cfd624e7cd478c3cad9
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-05 02:54:11 -0300

    1037 feito

--- beecrowd/pagina01/1037.js
+++ beecrowd/pagina01/1037.js
@@ -1,3 +1,13 @@
-const nmr = parseFloat(25.01);
-//  ([0,25], (25,50], (50,75], (75,100])
-if (nmr >= 0 && )
+const nmr = parseFloat(lines[0]);
+
+if (nmr >= 0 && nmr <= 25.00) {
+    console.log('Intervalo [0,25]')
+} else if (nmr > 25.00 && nmr <= 50.00) {
+        console.log('Intervalo (25,50]');
+    } else if (nmr > 50.00 && nmr <= 75.00) {
+        console.log('Intervalo (50,75]');
+    } else if (nmr > 75.00 && nmr <= 100.00) {
+        console.log('Intervalo (75,100]');
+    } else {
+        console.log('Fora de intervalo');
+    }
