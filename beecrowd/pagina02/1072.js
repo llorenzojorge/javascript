136557fd7b72afeccdc841f05d5c8649ef079d8c
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-16 14:15:41 -0300

    1072 resolvido

--- beecrowd/pagina02/1072.js
+++ beecrowd/pagina02/1072.js
@@ -3,17 +3,14 @@ let n = parseInt(lines[0]);
 
 function receberValores(nmr) {
     let arrayX = [];
-    let inside;
-    let outside;
+    let inside = 0;
+    let outside = 0;
 
     for (let i = 1; i <= nmr; i++) {
         arrayX.push(lines[i]);
     }
-
-    let intArray = arrayX.map(item => parseInt(item));
-
     for (let i = 0; i < nmr; i++) {
-        if (intArray[i] >= 10 && intArray[i] <= 20) {
+        if (arrayX[i] >= 10 && arrayX[i] <= 20) {
             inside++
         } else {
             outside++
@@ -22,61 +19,10 @@ function receberValores(nmr) {
 
     return {
         in: inside,
-        on: outside
+        out: outside
     }
 }
 
 let res = receberValores(n);
-console.log(`${res.inside} in`);
-console.log(`${res.outside} out`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let n = parseInt(lines[0]);
-// let n1 = parseInt(lines[1]);
-// let n2 = parseInt(lines[2]);
-// let n3 = parseInt(lines[3]);
-// let n4 = parseInt(lines[4]);
-
-// let inside = 0;
-// let outside = 0;
-
-// function dentroFora(numero) {
-//     if (numero >= 10 && numero <= 20) {
-//         inside++
-//     } else {
-//         outside++
-//     }    
-// }
-
-// dentroFora(n1);
-// dentroFora(n2);
-// dentroFora(n3);
-// dentroFora(n4);
-
-// console.log(`${inside} in`);
-// console.log(`${outside} in`);
+console.log(`${res.in} in`);
+console.log(`${res.out} out`);
