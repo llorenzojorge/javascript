9b2c2c66df96ba45e42818045577c990d07014a1
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-19 16:49:49 -0300

    1095 feito
    o erro estava sendo ao executar a função dentro do console, sendo que era só ativar a função "sequenciaIJ()"

--- beecrowd/pagina03/1095.js
+++ beecrowd/pagina03/1095.js
@@ -2,11 +2,10 @@ function sequenciaIJ() {
     let saidaI = parseInt(1);
     let saidaJ = parseInt(60);
 
-    for (let i = 1; saidaJ !== 0; i++) {
+    while (saidaJ >= 0) {
         console.log(`I=${saidaI} J=${saidaJ}`);
         saidaI+=3;
         saidaJ-=5;
     }
 }
-const res = sequenciaIJ();
-console.log(res);
+sequenciaIJ();
