73ecf75836bc68416c03906f1f373ad17f0da9fb
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-22 17:12:35 -0300

    Update 1080.js

--- beecrowd/pagina03/1080.js
+++ beecrowd/pagina03/1080.js
@@ -1,25 +1,17 @@
 let lines = [2, 113, 45, 34565, 923, 923, 33323413]
 
-function maiorEPosicao() {
-    let maior = lines[0];
-    let posicao = 1
+let maior = lines[0];
+let posicao = 1;
 
-    for (let i = 1; i < lines.length; i++) {
-        if (lines[i] > maior) {
-            maior = lines[i];
-            posicao = i + 1;
-        }
-    }
-
-    return {
-        maior: maior,
-        posicao: posicao
+for (let i = 1; i < lines.length; i++) {
+    if (lines[i] > maior) {
+        maior = lines[i];
+        posicao = i + 1;
     }
 }
 
-const res = maiorEPosicao();
-console.log(res.maior);
-console.log(res.posicao);
+console.log(maior);
+console.log(posicao);
 
 
 
