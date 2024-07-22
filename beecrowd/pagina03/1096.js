b036c53da2000dd077b75a8075d479fe958953cb
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-22 17:27:20 -0300

    1096 feito

--- beecrowd/pagina03/1096.js
+++ beecrowd/pagina03/1096.js
@@ -2,14 +2,14 @@ function sequenciaIJ() {
     let saidaI = parseInt(1);
     let saidaJ = parseInt(7);
 
-    while (saidaI !== 9) {
+    while (saidaI !== 11) {
         console.log(`I=${saidaI} J=${saidaJ}`);
-        saidaJ--
+        saidaJ--;
         console.log(`I=${saidaI} J=${saidaJ}`);
-        saidaJ--
+        saidaJ--;
         console.log(`I=${saidaI} J=${saidaJ}`);
         saidaI = saidaI +2;
-        saidaJ = 7
+        saidaJ = 7;
     }
 }
 sequenciaIJ();
