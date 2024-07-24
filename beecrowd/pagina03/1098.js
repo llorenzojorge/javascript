523bd32c10f2b746f14dabafcbc22f312d3cabd0
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-24 18:05:32 -0300

    PASSANDO RAIVA NO 1098

--- beecrowd/pagina03/1098.js
+++ beecrowd/pagina03/1098.js
@@ -2,11 +2,11 @@ let nI = parseInt(0);
 let nJ = parseInt(1);
 
 for (let i = 1; nI < 2.1; i++) {
-    console.log(`I=${nI} J=${nJ}`);
+    console.log(`I=${nI}`);
     nJ++
-    console.log(`I=${nI} J=${nJ}`);
+    //console.log(`I=${nI}`);
     nJ++
-    console.log(`I=${nI} J=${nJ}`);
+    //console.log(`I=${nI}`);
     nJ-=1.8
     nI+=0.2
 }
