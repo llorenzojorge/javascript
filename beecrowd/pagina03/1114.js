41e118da958a1dc4774d385355c69aa3a55cad2a
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 03:39:14 -0300

    1114 feito

--- beecrowd/pagina03/1114.js
+++ beecrowd/pagina03/1114.js
@@ -1,9 +1,9 @@
-let lines = [2200, 1020, 2022, 2002]
+// let lines = [2200, 1020, 2022, 2002, 2003]
 
-for (let i = 0; i > lines.length; i++) {
-    if (lines[i] === 2002) {
+for (let i = 0; i <= lines.length; i++) {
+    if (lines[i] == 2002) {
         console.log('Acesso Permitido');
-        return
+        break;
     } else {
         console.log('Senha Invalida');
     }
