d58dc073f3538d8b6a283c5b3ac06cd7ce7f03d1
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 03:36:08 -0300

    1114 em andamento
    troquei o return por break

--- beecrowd/pagina03/1114.js
+++ beecrowd/pagina03/1114.js
@@ -1,9 +1,10 @@
-let lines = 
-for (let i = 0; 1 > 0; i++) {
-    if (lines[i] !== 2002) {
-        console.log('Senha Invalida');
-    } else {
+let lines = [2200, 1020, 2022, 2002]
+
+for (let i = 0; i > lines.length; i++) {
+    if (lines[i] === 2002) {
         console.log('Acesso Permitido');
-        return;
+        return
+    } else {
+        console.log('Senha Invalida');
     }
 }
