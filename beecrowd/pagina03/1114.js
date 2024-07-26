b91b527e0b6a99d4ef0d72d7e849aa0122f15dc7
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 03:31:43 -0300

    Create 1114.js

---
+++ beecrowd/pagina03/1114.js
@@ -0,0 +1,9 @@
+let lines = 
+for (let i = 0; 1 > 0; i++) {
+    if (lines[i] !== 2002) {
+        console.log('Senha Invalida');
+    } else {
+        console.log('Acesso Permitido');
+        return;
+    }
+}
