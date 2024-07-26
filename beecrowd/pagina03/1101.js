499bfdc86e0bcef2e6622f5ba49df5a9fbc1e8ae
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 04:32:18 -0300

    Update 1101.js

--- beecrowd/pagina03/1101.js
+++ beecrowd/pagina03/1101.js
@@ -8,16 +8,24 @@ for (let i = 0; i < lines.length; i++) {
     if (m <= 0 || n <= 0) {
         break;
     } else if (m > n) { // 5 > 2 true
+        let total = 0;
         for (let i = n; i <= n; i++) { // i = 2, 2 <= 5, 2++
-            let total = i
             total++
-            console.log(`${i} ${total}`)
+            console.log(`${i} ${total}`);
         }
     } else if (n > m) {
+        let total = 0;
         for (let i = m; i <= m; i++) {
-            let total = i
             total++
-            console.log(`${i} ${total}`)
+            console.log(`${i} ${total}`);
         }
     }
 }
+
+// function range(start, end, step = 1) {
+//     const arr = [];
+//     for (let i = start; i < end; i += step) {
+//         arr.push(i);
+//     }
+//     return arr;
+// }
