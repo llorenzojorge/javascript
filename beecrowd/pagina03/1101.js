8d455b097ebc949a181c28f2df47344bc311cfe7
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 03:59:48 -0300

    Update 1101.js

---
+++ beecrowd/pagina03/1101.js
@@ -0,0 +1,7 @@
+let lines = ['5 2', '6 3', '5 0'];
+
+for (let i = 0; i < lines.length; i++) {
+    let entrada = lines[i].split(' ');
+    let m = parseInt(entrada[0]);
+    let n = parseInt(entrada[1]);
+}
