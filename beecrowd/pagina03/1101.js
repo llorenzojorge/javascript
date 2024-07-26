2f0b8b02765ff7118527e61927da35b4d32249a4
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 04:10:58 -0300

    Update 1101.js

--- beecrowd/pagina03/1101.js
+++ beecrowd/pagina03/1101.js
@@ -4,4 +4,20 @@ for (let i = 0; i < lines.length; i++) {
     let entrada = lines[i].split(' ');
     let m = parseInt(entrada[0]);
     let n = parseInt(entrada[1]);
+
+    if (m <= 0 || n <= 0) {break;}
+
+    else if (m > n) {
+        for (let i = n; m >= n; i++) {
+            let total = 0
+            total+=i
+            console.log(i + total)
+        }
+    } else if (n > m) {
+        for (let i = m; n >= m; i++) {
+            let total = 0
+            total+=i
+            console.log(i + total)
+        }
+    }
 }
