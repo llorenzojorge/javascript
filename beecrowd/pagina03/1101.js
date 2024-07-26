52ec160b68f25c6588cf0775ff9091665ea74eb8
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-26 04:22:11 -0300

    1101 em andamento

--- beecrowd/pagina03/1101.js
+++ beecrowd/pagina03/1101.js
@@ -5,19 +5,19 @@ for (let i = 0; i < lines.length; i++) {
     let m = parseInt(entrada[0]);
     let n = parseInt(entrada[1]);
 
-    if (m <= 0 || n <= 0) {break;}
-
-    else if (m > n) {
-        for (let i = n; m >= n; i++) {
-            let total = 0
-            total+=i
-            console.log(i + total)
+    if (m <= 0 || n <= 0) {
+        break;
+    } else if (m > n) { // 5 > 2 true
+        for (let i = n; i <= n; i++) { // i = 2, 2 <= 5, 2++
+            let total = i
+            total++
+            console.log(`${i} ${total}`)
         }
     } else if (n > m) {
-        for (let i = m; n >= m; i++) {
-            let total = 0
-            total+=i
-            console.log(i + total)
+        for (let i = m; i <= m; i++) {
+            let total = i
+            total++
+            console.log(`${i} ${total}`)
         }
     }
 }
