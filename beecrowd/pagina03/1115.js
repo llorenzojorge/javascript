5376e09e073d3f9194582ecddf208c7e0abe60e9
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-27 17:15:38 -0300

    Update 1115.js

--- beecrowd/pagina03/1115.js
+++ beecrowd/pagina03/1115.js
@@ -1,4 +1,17 @@
-let entrada = lines[0].split(' ');
-let x = parseInt(entrada[0]);
-let y = parseInt(entrada[1]);
+for (let i = 0; i < lines.length; i++) {
+    let entrada = lines[i].split(' ');
+    let x = parseInt(entrada[0]);
+    let y = parseInt(entrada[1]);
 
+    if (x > 0 && y > 0) {
+        console.log('primeiro');
+    } else if (x < 0 && y > 0) {
+        console.log('segundo');
+    } else if (x < 0 && y < 0) {
+        console.log('terceiro');
+    } else if (x > 0 && y < 0) {
+        console.log('quarto');
+    } else if (x === 0 || y === 0) {
+        break;
+    }
+}
