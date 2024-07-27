7ce76e6b35120e3ff2ffc96bb7521895d41cc5a4
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-27 17:26:37 -0300

    Update 1116.js

--- beecrowd/pagina03/1116.js
+++ beecrowd/pagina03/1116.js
@@ -1,5 +1,15 @@
-for (let i = 0; i < lines.length; i++) {
+let lines = [3, '3 -2', '-8 0', '0 8']
+const entrada = lines[0];
+
+for (let i = 1; i <= entrada; i++) {
     let entrada = lines[i].split(' ');
     let x = parseInt(entrada[0]);
     let y = parseInt(entrada[1]);
+
+    let divisao = x / y;
+    if (!isFinite(divisao)) {
+        console.log('divisao impossivel')
+    } else {
+        console.log(divisao)
+    }
 }
