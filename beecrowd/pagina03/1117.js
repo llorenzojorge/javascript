08d137c516488b0fd502badd0a3b75856c4bc23c
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-29 03:00:07 -0300

    Update 1117.js

--- beecrowd/pagina03/1117.js
+++ beecrowd/pagina03/1117.js
@@ -1,12 +1,19 @@
+let lines = [-3.5, 3.5, 11.0, 10.0];
+let nota1; 
+let nota2;
+let media = (nota1 + nota2) / 2 
+
 for (let i = 0; i < lines.length; i++) {
-    let nota = lines[i]
-    let media = (nota1 + nota2) / 2
-    
+    let nota = lines[i];
+
     if (nota < 0 || nota > 10) {
         console.log('nota invalida');
-        return;
-    } else {
-
+    } else if (nota1 === undefined) {
+        nota1 = lines[i];
+        console.log(nota1)
+    } else if (nota1 != undefined) {
+        nota2 = lines[i]
+        console.log(`media = ${media.toFixed(2)}`)
     }
 }
 
