6ab6449ea9ac07a3b054dfba44d3e15477ce0dfe
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-07-29 22:48:09 -0300

    1094 decidir usar function

--- beecrowd/pagina03/1094.js
+++ beecrowd/pagina03/1094.js
@@ -1,11 +1,16 @@
 let lines = [10, '10 C', '6 R', '15 S', '5 C', '14 R', '9 C', '6 R', '8 S', '5 C', '14 R']
-let entrada = parseInt(lines[0]);
-let cobaiaQtd; let cobaiaTipo;
+function experiencias(entrada) {
+    let cobaiaQtd; let cobaiaTipo;
+    let total; let totalC; let totalR; let totalS
+    let percentualC; let percentualR; let percentualS;
 
-for(let i = 1; i <= entrada; i++) {
-    // separando a QTD e TIPO de cobaias
-    let separacao = lines[i].split(' ');
-    cobaiaQtd = parseInt(separacao[0]);
-    cobaiaTipo = separacao[1];
-    console.log(cobaiaQtd + cobaiaTipo)
+    for(let i = 1; i <= entrada; i++) {
+        // separando a QTD e TIPO de cobaias
+        let separacao = lines[i].split(' ');
+        cobaiaQtd = parseInt(separacao[0]);
+        cobaiaTipo = separacao[1];
+    }
 }
+
+let entrada = parseInt(lines[0]);
+experiencias(entrada);
