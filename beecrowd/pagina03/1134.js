d3f64898ce39333726f939340c7e34b7a9e499e4
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-03 14:22:39 -0300

    Update 1134.js

--- beecrowd/pagina03/1134.js
+++ beecrowd/pagina03/1134.js
@@ -5,4 +5,21 @@ function tipoCombustivel() {
     let alcool = parseInt(0);
     let gasolina = parseInt(0);
     let diesel = parseInt(0);
+
+    for (let i = 0; i < lines.length; i++) {
+        entrada = parseInt(lines[i]);
+
+        if (entrada <= 0 || entrada >= 5) {
+            break
+        } else if (entrada === 1) {
+            alcool++
+        } else if (entrada === 2) {
+            gasolina++
+        } else if (entrada === 3) {
+            diesel++
+        } else { // entrada: 4
+            break
+        }
+    }
+    
 }
