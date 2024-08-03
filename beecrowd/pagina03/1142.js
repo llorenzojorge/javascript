df740053d203849779977071b26b916c03362e9f
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-03 14:38:32 -0300

    1142 feito

--- beecrowd/pagina03/1142.js
+++ beecrowd/pagina03/1142.js
@@ -1,13 +1,13 @@
 let lines = [7];
 let entrada = parseInt(lines[0]);
 
+function pum() {
+    let n1 = parseInt(1);
+    for (let i = 0; i < entrada; i++) {
+        let y = 1;
+        console.log(`${n1} ${n1 + 1} ${n1 + 2} PUM`);
+        n1+=4;
+    }
+}
 
-
-
-// 1 2 3 PUM
-// 5 6 7 PUM
-// 9 10 11 PUM
-// 13 14 15 PUM
-// 17 18 19 PUM
-// 21 22 23 PUM
-// 25 26 27 PUM
+pum();
