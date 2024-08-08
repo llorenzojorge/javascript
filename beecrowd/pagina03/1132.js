5bde35e0c7a1ce856ef8241172d334ba240c6e24
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-08 14:00:49 -0300

    Update 1132.js

--- beecrowd/pagina03/1132.js
+++ beecrowd/pagina03/1132.js
@@ -1,17 +1,15 @@
-// let lines = [100, 200]
-// const x = parseInt(lines[0]);
-// const y = parseInt(lines[1]);
+const x = parseInt(lines[0]);
+const y = parseInt(lines[1]);
 
-// function multiplos() {
-//     let total = 0;
+function multiplos() {
+    let total = 0;
 
-//     for (let i = x; x <= y; i++) {
-//         if (i % 13 !== 0) {
-//             total+=i;
-//         }
-//     }
-//     console.log(total);
-// }
+    for (let i = x; i <= y; i++) {
+        if (i % 13 !== 0) {
+            total+=i;
+        }
+    }
+    console.log(total);
+}
 
-// multiplos();
-console.log('oi')
+multiplos();
