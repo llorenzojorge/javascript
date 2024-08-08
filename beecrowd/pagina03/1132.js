7f6d558b5aacde7134c932abfeebd8f0cec53c61
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-08 14:06:25 -0300

    1132 feito

--- beecrowd/pagina03/1132.js
+++ beecrowd/pagina03/1132.js
@@ -1,10 +1,20 @@
+let lines = [200, 100]
 const x = parseInt(lines[0]);
 const y = parseInt(lines[1]);
 
 function multiplos() {
-    let total = 0;
+    let total = parseInt(0);
+    let maior; let menor;
 
-    for (let i = x; i <= y; i++) {
+    if (x > y) {
+        maior = x;
+        menor = y;
+    } else {
+        maior = y;
+        menor = x;
+    }
+
+    for (let i = menor; i <= maior; i++) {
         if (i % 13 !== 0) {
             total+=i;
         }
